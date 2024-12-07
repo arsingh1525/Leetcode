fb9a1aa3a8476982862273f3e53ff1d42f73d73c
Ankit Kumar <arsingh1525@gmail.com>
2024-12-07 08:49:38 +0530

    Time: 11 ms (6.71%), Space: 45.1 MB (6.78%) - LeetHub

---
+++ 0443-string-compression/0443-string-compression.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int compress(char[] arr) {
+        String ans="";
+        int i=0,j=0;
+        while(j<arr.length){
+            if(arr[j]==arr[i])j++;
+            else{
+                ans+=arr[i];
+                int len=j-i;
+                if(len>1)ans+=len;
+                i=j;
+
+            }
+           
+            }
+             ans+=arr[i];
+            int len=j-i;
+            if(len>1)ans+=len;
+            for(i=0;i<ans.length();i++){
+                arr[i]=ans.charAt(i);
+            
+        }
+        return ans.length();
+
+    }
+}
